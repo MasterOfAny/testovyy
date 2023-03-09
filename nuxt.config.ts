605b7670b743fa10a2d8eb06954d9b613450d2d6
Nik <77840519+MasterOfAny@users.noreply.github.com>
2023-03-09 15:58:10 +0300

    up nuxtconf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const isDev = process.env.NODE_ENV !== 'production'
 export default defineNuxtConfig({
-
+    build: {
+        transpile: !isDev ? [
+            'yup',
+            'maska',
+            'lodash',
+            'lodash-es',
+            '@vee-validate/rules'
+        ] : []
+    },
+    runtimeConfig: {
+        baseURL: 'https://google.com'
+    },
 })
